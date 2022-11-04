f78ff8b7a5c48f1422d65938f434cfa0dc240651
b01010110 <b01010110b@gmail.com>
2022-11-04 21:35:14 +0700

    vite plugin fonts has been setup

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 import vuetify from '@/plugins/vuetify'
-import { loadFonts } from '@/plugins/webfontloader'
 
 import 'virtual:svg-icons-register'
 import '@/assets/scss/main.scss'
@@ -12,7 +11,5 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
-
-loadFonts()
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import Components from 'unplugin-vue-components/vite'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 export default defineConfig({
   plugins: [
@@ -17,6 +18,14 @@ export default defineConfig({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
     }),
     Components(),
+    VitePluginFonts({
+      google: {
+        families: [
+          'Fira+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700',
+          'Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
