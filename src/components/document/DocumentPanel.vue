d312cdcd111d148fad6a8e9a8c04c83547d26092
b01010110 <b01010110b@gmail.com>
2022-11-05 18:38:42 +0700

    document panel component has been added

---
+++ src/components/document/DocumentPanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="document-panel">
+    <div class="document-panel__title">Документы</div>
+    <v-spacer />
+    <div class="document-panel__buttons">
+      <AppBtn class="parent-svg-icon-color--grey" svg-icon="bookmark" icon></AppBtn>
+      <AppBtn class="parent-svg-icon-color--blue" svg-icon="plus">Новый тип</AppBtn>
+      <AppBtn class="parent-svg-icon-color--blue" svg-icon="plus">Новый документ</AppBtn>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.document-panel {
+  display: flex;
+
+  &__title {
+    font-family: 'Fira Sans', sans-serif;
+    font-size: 22px;
+    font-weight: 500;
+    line-height: 108%;
+    color: #000;
+  }
+
+  &__buttons > button {
+    margin-right: 10px;
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+}
+</style>
