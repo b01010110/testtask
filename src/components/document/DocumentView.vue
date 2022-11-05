a5362ac922a3f1b2e9726b424d4c3e4841f6e252
b01010110 <b01010110b@gmail.com>
2022-11-05 18:41:12 +0700

    document view component has been added

---
+++ src/components/document/DocumentView.vue
@@ -0,0 +1,15 @@
+<template>
+  <DocumentPanel />
+  <DocumentSearch class="document-view__search" />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.document-view {
+  &__search {
+    margin-top: 23px;
+    max-width: 564px;
+  }
+}
+</style>
