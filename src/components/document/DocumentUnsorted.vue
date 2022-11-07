6bacd34491b3a0c3d8dfb67115969f0624377fd0
b01010110 <b01010110b@gmail.com>
2022-11-07 13:20:20 +0700

    DocumentUnsorted component has been added

---
+++ src/components/document/DocumentUnsorted.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="document-unsorted">
+    <DocumentDocument v-for="document in documents" :key="document.id" :document="document" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGlobalState } from '@/store'
+
+const { unsorted: documents } = useGlobalState()
+</script>
+
+<style scoped lang="scss">
+.document-unsorted {
+  & > div {
+    margin-top: -1px;
+
+    &:first-of-type {
+      margin-top: 0;
+    }
+  }
+}
+</style>
