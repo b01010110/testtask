0caebdf0c76089972a24e1b22af8a7ebd06ff567
b01010110 <b01010110b@gmail.com>
2022-11-07 13:17:42 +0700

    DocumentGroups component has been added

---
+++ src/components/document/DocumentGroups.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-expansion-panels v-model="data" class="document-groups" variant="accordion" :multiple="multiple">
+    <slot></slot>
+  </v-expansion-panels>
+</template>
+
+<script setup lang="ts">
+import { provide } from 'vue'
+import { useVModel } from '@vueuse/core'
+import { documentGroupsValueKey, documentGroupsOpenKey, documentGroupsCloseKey } from '@/keys'
+
+interface DocumentGroupsProps {
+  modelValue: string[] | string
+  multiple?: boolean
+}
+
+interface DocumentGroupsEmits {
+  (e: 'update:modelValue', modelValue: string[] | string): void
+}
+
+const props = withDefaults(defineProps<DocumentGroupsProps>(), {
+  multiple: false,
+})
+const emit = defineEmits<DocumentGroupsEmits>()
+
+const data = useVModel(props, 'modelValue', emit)
+
+function open(value: string) {
+  if (props.multiple) {
+    ;(data.value as string[]).push(value)
+  } else {
+    data.value = value
+  }
+}
+
+function close(value?: string) {
+  if (props.multiple) {
+    data.value = (data.value as string[]).filter((val) => val !== value)
+  } else {
+    data.value = ''
+  }
+}
+
+provide(documentGroupsValueKey, data)
+provide(documentGroupsOpenKey, open)
+provide(documentGroupsCloseKey, close)
+</script>
+
+<style scoped lang="scss">
+.document-groups {
+  & > :deep(div) {
+    margin-top: -1px;
+
+    &:first-of-type {
+      margin-top: 0;
+    }
+  }
+}
+</style>
