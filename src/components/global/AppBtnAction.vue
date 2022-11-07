63e38bbd9b61239cce93fdc4b2c1f677bb3bb208
b01010110 <b01010110b@gmail.com>
2022-11-07 13:12:42 +0700

    AppBtnAction component has been added

---
+++ src/components/global/AppBtnAction.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn class="app-btn-action" icon flat>
+    <SvgIcon v-if="svgIcon" :name="svgIcon" />
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+interface AppBtnActionProps {
+  svgIcon?: string
+}
+
+withDefaults(defineProps<AppBtnActionProps>(), {
+  svgIcon: '',
+})
+</script>
+
+<style scoped lang="scss">
+.app-btn-action {
+  width: 26px !important;
+  height: 26px !important;
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+}
+</style>
