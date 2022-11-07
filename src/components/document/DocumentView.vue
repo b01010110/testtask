ad8e3de7778ba77e93f94397823563635874f8e3
b01010110 <b01010110b@gmail.com>
2022-11-07 13:24:28 +0700

    DocumentSorted and DocumentUnsorted components have been inserted

--- src/components/document/DocumentView.vue
+++ src/components/document/DocumentView.vue
@@ -1,6 +1,8 @@
 <template>
   <DocumentPanel />
   <DocumentSearch class="document-view__search" />
+  <DocumentSorted class="document-view__sorted" />
+  <DocumentUnsorted class="document-view__unsorted" />
 </template>
 
 <script setup lang="ts"></script>
@@ -10,6 +12,16 @@
   &__search {
     margin-top: 23px;
     max-width: 564px;
+  }
+
+  &__sorted {
+    margin-top: 19px;
+    max-width: 1190px;
+  }
+
+  &__unsorted {
+    margin-top: 14px;
+    max-width: 1190px;
   }
 }
 </style>
