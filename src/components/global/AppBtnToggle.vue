59afb392f7cbeb62bfcad268c103c9c5599a1b49
b01010110 <b01010110b@gmail.com>
2022-11-07 13:12:12 +0700

    AppBtnToggle component has been added

---
+++ src/components/global/AppBtnToggle.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn class="app-btn-toggle" :class="{ 'app-btn-toggle--expanded': isExpanded }" icon="mdi-chevron-down" flat />
+</template>
+
+<script setup lang="ts">
+interface AppBtnToggleProps {
+  isExpanded?: boolean
+}
+
+withDefaults(defineProps<AppBtnToggleProps>(), {
+  isExpanded: false,
+})
+</script>
+
+<style scoped lang="scss">
+.app-btn-toggle {
+  width: 22px !important;
+  height: 22px !important;
+  border: 1px solid #d3d8df;
+  font-size: 11px;
+
+  :deep(i) {
+    position: relative;
+    top: 1px;
+    transition: all 0.3s;
+  }
+
+  &--expanded {
+    :deep(i) {
+      top: -1px;
+      transform: rotate(180deg);
+    }
+  }
+}
+</style>
